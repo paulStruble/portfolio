1367e9a0e3b8065c12f91bfc340014377fd99308
Paul Struble <pjstruble1@gmail.com>
2024-07-01 22:48:19 -0700

    Initial framework

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000; // default port: 3000
+
+// Set public directory as static root
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Default endpoint
+app.get('/', (req, res) => {
+    
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+
+});
+
+// Start app
+app.listen(port, () => {
+    console.log(`App listening on port ${port} ...`);
+});
+
+// On exit:
+process.on('exit', () => {
+    console.log(`Process exited on port: ${port}`);
+});
