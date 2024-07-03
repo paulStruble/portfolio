0128674c181b16d598a04d953095e945cc7721dc
Paul Struble <pjstruble1@gmail.com>
2024-07-02 18:33:20 -0700

    Poppins font, index.html layout

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Default endpoint
 app.get('/', (req, res) => {
     
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 
 });
 
