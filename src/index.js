6f161a86f3ed28a72e08800df0c5dce4352b355b
Paul Struble <pjstruble1@gmail.com>
2024-07-07 18:14:27 -0700

    prettier (extension) implementation

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Default endpoint
 app.get('/', (req, res) => {
-    
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-
 });
 
 // Start app
