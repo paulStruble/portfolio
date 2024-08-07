0446ddb25f396cf868585ffd82665dc2b4bee1db
Paul Struble <pjstruble1@gmail.com>
2024-08-06 23:47:08 -0700

    404 error page

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ const port = process.env.PORT || 3000; // default port: 3000
 // Set public directory as static root
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Handle 404 errors
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
 // Default endpoint
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
